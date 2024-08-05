e839c6f39e4b5f48d8a5084fe4b9d0238516bc86
IsaiasRB123 <isaiwenhill@gmail>
2024-08-05 07:28:20 -0500

    Token re-acabado

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,38 +1,34 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { createUser, findUserByEmail } = require('../repositories/UserRepository');
-const { JWT_SECRET } = process.env;
+const bcrypt = require('bcrypt');
+const jwtUtils = require('../utils/jwtUtils');
+const userRepository = require('../repositories/UserRepository');
 
 const register = async (name, email, password, phone, roleId) => {
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await createUser({
-    name,
-    email,
-    password: hashedPassword,
-    phone,
-    roleId
-  });
-  return user;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await userRepository.createUser({
+        name,
+        email,
+        password: hashedPassword,
+        phone,
+        roleId
+    });
+    const token = jwtUtils.generateToken(user);
+    return { user, token };
 };
 
 const login = async (email, password) => {
-  console.log('Iniciando sesión para el email:', email); // Añadir log
-  const user = await findUserByEmail(email);
-  if (!user) {
-    console.log('Usuario no encontrado'); // Añadir log
-    throw new Error('Usuario no encontrado');
-  }
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) {
-    console.log('Contraseña inválida'); // Añadir log
-    throw new Error('Contraseña inválida');
-  }
-  const token = jwt.sign({ id: user.id, email: user.email, roleId: user.roleId }, JWT_SECRET, { expiresIn: '1h' });
-  console.log('Usuario autenticado:', user); // Añadir log
-  return { user, token };
+    const user = await userRepository.findUserByEmail(email);
+    if (!user) {
+        throw new Error('Usuario no encontrado');
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        throw new Error('Contraseña incorrecta');
+    }
+    const token = jwtUtils.generateToken(user);
+    return { user, token };
 };
 
 module.exports = {
-  register,
-  login
+    register,
+    login
 };
