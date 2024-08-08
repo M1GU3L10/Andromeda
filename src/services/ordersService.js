ec5353e4522f96f3c9976c1e0d606dfbdf357184
Steven11vm <stevenvilla10@gmail.com>
2024-08-07 19:22:23 -0500

    opium899

--- src/services/ordersService.js
+++ src/services/ordersService.js
@@ -1,4 +1,5 @@
 const orderRepository = require('../repositories/ordersRepository');
+const salesRepository = require('../repositories/saleRepository');
 
 const getAllOrders = async () => {
     return await orderRepository.getAllOrders();
@@ -9,15 +10,29 @@ const getOrderById = async (id) => {
 };
 
 const createOrder = async (data) => {
-    return await orderRepository.createOrder(data);
+    const newOrder = await orderRepository.createOrder(data);
+    await checkAndConvertToSale(newOrder);
+    return newOrder;
 };
 
 const updateOrder = async (id, data) => {
-    return await orderRepository.updateOrder(id, data);
+    const updatedOrder = await orderRepository.updateOrder(id, data);
+    await checkAndConvertToSale(updatedOrder);
+    return updatedOrder;
 };
 
 const deleteOrder = async (id) => {
     return await orderRepository.deleteOrder(id);
+};
+
+const checkAndConvertToSale = async (order) => {
+    if (order.status === 'correcto') {
+        const saleData = {
+            orderId: order.id,
+            ...order
+        };
+        await salesRepository.createSale(saleData);
+    }
 };
 
 module.exports = {
