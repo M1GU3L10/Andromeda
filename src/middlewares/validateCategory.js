e4efc4ab9a9983f4292da6a6b0de29eddc9699fe
M1GU3L10 <123miguelfps@gmail.com>
2024-08-30 22:40:58 -0500

    Ah no chauu

--- src/middlewares/validateCategory.js
+++ src/middlewares/validateCategory.js
@@ -1,7 +1,7 @@
 const { body, validationResult } = require('express-validator');
 const { models } = require('../models');
 
-const validateCategory = [
+const validateCategoryRegister = [
     body('name').notEmpty().withMessage('El nombre es requerido')
     .custom(async (value) => {
         const category = await models.Category.findOne({ where: { name: value } });
@@ -21,4 +21,16 @@ const validateCategory = [
     }
 ];
 
-module.exports = validateCategory;
+const validateCategoryUpdated = [
+    body('status')
+        .isIn(['A', 'I']).withMessage('El estado debe ser A (Activo) o I (Inactivo)'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+];
+
+module.exports = {validateCategoryRegister,validateCategoryUpdated};
