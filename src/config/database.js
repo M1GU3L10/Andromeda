95d5080891196e2fce3736a4d3906a0781bfe7c6
joeltd15 <joeltuiran15@gmail.com>
2024-12-11 21:53:27 -0500

    aaaaaaaaaa

--- src/config/database.js
+++ src/config/database.js
@@ -1,26 +1,26 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-// Configuración de la conexión a la base de datos usando variables de entorno
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT, 10), // Convierte el puerto a número entero
+        port: parseInt(process.env.DB_PORT, 10),
         dialect: process.env.DB_DIALECT,
-        logging: false, // Desactiva los logs de Sequelize
+        logging: false,
         pool: {
-            max: 5, // Máximo número de conexiones en el pool
-            min: 0, // Mínimo número de conexiones en el pool
-            acquire: 30000, // Tiempo máximo de espera para adquirir una conexión
-            idle: 10000, // Tiempo máximo de inactividad de una conexión antes de cerrarla
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000,
         },
-        // Opciones específicas para el dialecto (en este caso, SSL)
         dialectOptions: {
-            ssl: true,
-            rejectUnauthorized: false,
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
         },
     }
 );
