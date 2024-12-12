e3ac6c07255df19488a1e68a86d11572d7a16416
joeltd15 <joeltuiran15@gmail.com>
2024-12-11 21:07:48 -0500

    uy

--- src/config/database.js
+++ src/config/database.js
@@ -7,8 +7,8 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT, // Asegúrate de incluir el puerto
-        dialect: 'mysql',
+        port: parseInt(process.env.PORT, 10), // Use PORT from .env, parsed as integer
+        dialect: process.env.DB_DIALECT, // Use DB_DIALECT from .env
         logging: false,
         pool: {
             max: 5,
